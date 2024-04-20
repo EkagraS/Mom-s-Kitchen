dfcfc4df0dba3b47d4aeed4ebc145526fd9c3689
Ekagra Shandilya <shandilyaekagra@gmail.com>
2024-04-21 03:33:25 +0530

    First commit

--- app/src/main/java/com/example/momskitchen/ProfileFragment.java
+++ app/src/main/java/com/example/momskitchen/ProfileFragment.java
@@ -4,10 +4,8 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +15,6 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
